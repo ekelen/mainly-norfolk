e700893bfb78cbe06f4a706b5c35cfb5c4ffd4b4
Elizabeth Kelen <ekelen@Elizabeths-MacBook-Pro.local>
2022-01-28 09:45:57 -0500

    hook up function

--- functions/get-music.js
+++ functions/get-music.js
@@ -1,14 +1,18 @@
 const fetch = require("node-fetch");
 
 exports.handler = async (event, context) => {
+  console.log(`[=] event:`, event);
+  console.log(`[=] context:`, context);
   const { href } = JSON.parse(event.body);
+  console.log(`[=] href:`, href);
   try {
     const response = await fetch(href + ".json");
     const data = await response.json();
     return { statusCode: 200, body: JSON.stringify({ data }) };
   } catch (error) {
+    console.log(`[=] error:`, error);
     return {
-      statusCode: error.statusCode,
+      statusCode: 500,
       body: { error: error.message },
     };
   }
